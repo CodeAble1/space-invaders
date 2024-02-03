b9fae7f1a60ff7ebe23ffca58cc09b962bccf643
CodeAble1 <calebcoding123@gmail.com>
2024-02-03 10:12:06 +0200

    Adding Start menu

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from email.base64mime import header_length
 import pygame
 import sys
 from random import randint
@@ -8,15 +7,19 @@ pygame.init()
 # Constants
 WIDTH,HEIGHT = 800, 600
 FPS = 60
+RUN = False
 
 # Setup
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invaders")
 background = pygame.image.load("assets/background-black.png").convert_alpha()
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+font = pygame.font.Font(None, 36)
 clock = pygame.time.Clock()
 enemy_timer = 60
 shooting_timer = [80, 120, 160]
+score = 0
+
 
 # Player
 class Player(pygame.sprite.Sprite):
@@ -48,6 +51,10 @@ class Player(pygame.sprite.Sprite):
 
     def damage(self):
         self.health -= 10
+    
+    def reset(self):
+        self.health = 100
+        self.rect.center = (400, 400)
 
     def update(self):
         self.health_bar()
@@ -131,6 +138,7 @@ def ProjectileCollision(enemy_projectile, player_projectile):
             if pygame.sprite.collide_mask(sprite1, sprite2):
                 enemy_projectile.remove(sprite1)
                 player_projectile.remove(sprite2)
+                return True
 
 def PlayerCollision(enemy, player):
     for sprite1 in enemy:
@@ -138,6 +146,7 @@ def PlayerCollision(enemy, player):
             if pygame.sprite.collide_mask(sprite1, sprite2):
                 enemy.remove(sprite1)
                 player.sprite.damage()
+                return True
                 
 #Groups
 player_group = pygame.sprite.GroupSingle()
@@ -146,7 +155,7 @@ enemy_group = pygame.sprite.Group()
 enemy_projectile = pygame.sprite.Group()
 
 ship = Player()
-player_group.add(ship)
+
 
 
 # Event Loop
@@ -156,63 +165,106 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE] and ship.timer <= 0:
-        proj = PlayerProjectile(ship.rect.x, ship.rect.y)
-        player_projectile.add(proj)
-        ship.timer = 60
 
-    # Enemy Logic
-    if enemy_timer <= 0:
-        enemy_ship = Enemy(randint(1, 3), randint(20, WIDTH-20), randint(-100, 0))
-        enemy_group.add(enemy_ship)
-        enemy_timer = 60
-    
-    for enemy in enemy_group.sprites():
-        if shooting_timer[0] <= 0 and enemy.type == 1:
-            enemy_proj = EnemyProjectiles(enemy.type, enemy.rect.centerx, enemy.rect.centery)
-            enemy_projectile.add(enemy_proj)
-            
-        if shooting_timer[1] <= 0 and enemy.type == 2:
-            enemy_proj = EnemyProjectiles(enemy.type, enemy.rect.centerx, enemy.rect.centery)
-            enemy_projectile.add(enemy_proj)
-           
-        if shooting_timer[2] <= 0 and enemy.type == 3:
-            enemy_proj = EnemyProjectiles(enemy.type, enemy.rect.centerx, enemy.rect.centery)
-            enemy_projectile.add(enemy_proj)
-
-    # Reset timers
-    if shooting_timer[0] <= 0:
-        shooting_timer[0] = 80
-    if shooting_timer[1] <= 0:
-        shooting_timer[1] = 120
-    if shooting_timer[2] <= 0:
-        shooting_timer[2] = 160
-    
-    # Collision
-    ProjectileCollision(player_projectile, enemy_projectile)
-    ProjectileCollision(enemy_group, player_projectile)
-    PlayerCollision(enemy_group, player_group)
-    PlayerCollision(enemy_projectile, player_group)
-
-    # Drawing
-    screen.blit(background, (0,0))
-    player_group.draw(screen)
-    player_projectile.draw(screen)
-    enemy_group.draw(screen)
-    enemy_projectile.draw(screen)
-  
-    
-    # Update
-    player_projectile.update()
-    enemy_projectile.update()
-    player_group.update()
-    enemy_group.update()
-    pygame.display.update()
-    
     clock.tick(FPS)
-    enemy_timer -= 0.5
-    for i, value in enumerate(shooting_timer):
-        shooting_timer[i] -= 1
+
+    # Start Menu
+    if not RUN:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            RUN = True
+            ship.reset()
+            score = 0
+            player_group.add(ship)
+    
+        # Drawing 
+        start_surface = font.render("Press SPACE to Start", False, (255, 255, 255))
+        start_rect = start_surface.get_rect(center = (WIDTH // 2, HEIGHT // 2))
+
+        prev_score_surface = font.render(f"Your Score was {score}", False, (255, 255, 255))
+        prev_score_rect = prev_score_surface.get_rect( center = (WIDTH // 2, (HEIGHT// 2) - 100))
+
+
+        # Update
+        screen.blit(background, (0,0))
+        screen.blit(start_surface, start_rect)
+        screen.blit(prev_score_surface, prev_score_rect)
+        pygame.display.update()
+        
+
+    # Game
+    if RUN:        
+        # Input
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and ship.timer <= 0:
+            proj = PlayerProjectile(ship.rect.x, ship.rect.y)
+            player_projectile.add(proj)
+            ship.timer = 60
+
+        
+
+        # Enemy Logic
+        if enemy_timer <= 0:
+            enemy_ship = Enemy(randint(1, 3), randint(50, WIDTH-50), randint(-100, 0))
+            enemy_group.add(enemy_ship)
+            enemy_timer = 60
+        
+        for enemy in enemy_group.sprites():
+            if shooting_timer[0] <= 0 and enemy.type == 1:
+                enemy_proj = EnemyProjectiles(enemy.type, enemy.rect.centerx, enemy.rect.centery)
+                enemy_projectile.add(enemy_proj)
+                
+            if shooting_timer[1] <= 0 and enemy.type == 2:
+                enemy_proj = EnemyProjectiles(enemy.type, enemy.rect.centerx, enemy.rect.centery)
+                enemy_projectile.add(enemy_proj)
+            
+            if shooting_timer[2] <= 0 and enemy.type == 3:
+                enemy_proj = EnemyProjectiles(enemy.type, enemy.rect.centerx, enemy.rect.centery)
+                enemy_projectile.add(enemy_proj)
+
+        # Reset timers
+        if shooting_timer[0] <= 0:
+            shooting_timer[0] = 80
+        if shooting_timer[1] <= 0:
+            shooting_timer[1] = 120
+        if shooting_timer[2] <= 0:
+            shooting_timer[2] = 160
+        
+        # Collision
+        ProjectileCollision(player_projectile, enemy_projectile)
+        if ProjectileCollision(enemy_group, player_projectile):
+            score += 1
+        if PlayerCollision(enemy_group, player_group):
+            score += 1
+        if PlayerCollision(enemy_projectile, player_group):
+            if player_group.sprite.health <= 0:
+                RUN = False
+                player_group.empty()
+                enemy_group.empty()
+                player_projectile.empty()
+                enemy_projectile.empty()
+
+
+        # Score
+        score_surface = font.render(f"Score {score}", True, (255, 255, 255))
+    
+        # Drawing
+        screen.blit(background, (0,0))
+        screen.blit(score_surface, (20, 20))
+        player_group.draw(screen)
+        player_projectile.draw(screen)
+        enemy_group.draw(screen)
+        enemy_projectile.draw(screen)
+    
+        
+        # Update
+        player_projectile.update()
+        enemy_projectile.update()
+        player_group.update()
+        enemy_group.update()
+        pygame.display.update()
+        
+        enemy_timer -= 0.5
+        for i, value in enumerate(shooting_timer):
+            shooting_timer[i] -= 1
 
